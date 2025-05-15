80d515afaf7929a856e5d6a79b5cf4c9df7a5410
vikasyadav01234 <yadav2911vikas@gmail.com>
2025-05-15 11:47:53 +0530

    low time complixity

--- digonal.java
+++ digonal.java
@@ -4,21 +4,27 @@ public class digonal {
 
     public static int digonalsum(int mat[][]){
         int sum = 0;
+        // for(int i=0;i<mat.length;i++){
+        //     for(int j=0;j<mat[0].length;j++){
+        //         if(i==j){
+        //             sum+=mat[i][j];
+        //         }
+        //     }
+        // }
+        // int digonal = mat[0].length;
+        // for(int i = 0;i<mat.length;i++){
+        //     sum+=mat[i][digonal-1];
+        //     digonal--;
+        // }
         for(int i=0;i<mat.length;i++){
-            for(int j=0;j<mat[0].length;j++){
-                if(i==j){
-                    sum+=mat[i][j];
-                }
+            sum+=mat[i][i];
+            if(i!=mat.length-1-i){
+                sum+=mat[i][mat.length-1-i];
             }
         }
-        int digonal = mat[0].length;
-        for(int i = 0;i<mat.length;i++){
-            sum+=mat[i][digonal-1];
-            digonal--;
-        }
-        if (mat.length%2==1) {
-            sum =sum-mat[(mat.length/2)][(mat.length/2)];
-        }
+        // if (mat.length%2==1) {
+        //     sum =sum-mat[(mat.length/2)][(mat.length/2)];
+        // }
         return sum;
     }
     public static void main(String arg[]){
