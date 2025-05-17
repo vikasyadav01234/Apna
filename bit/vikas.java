ea93db01d806f2fd71f1fd81676d556f098039d6
vikasyadav01234 <yadav2911vikas@gmail.com>
2025-05-17 12:20:55 +0530

    power using bit manipulation

---
+++ bit/vikas.java
@@ -0,0 +1,17 @@
+
+public class vikas {
+    public static int power(int a,int b){
+        int ans =1;
+        while (b>0){
+            if((1&b)!=0){
+                ans = ans*a;
+            }
+            a=a*a;
+            b = b>>1;
+        }
+        return ans;
+    }
+    public static void main(String [] args){
+        System.out.print(power(10, 5));
+    }
+}
