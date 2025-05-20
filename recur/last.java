bf635998f36dccd3fd4e22390d11c604944d42b2
vikasyadav01234 <yadav2911vikas@gmail.com>
2025-05-20 09:56:23 +0530

    last occers

--- recur/last.java
+++ recur/last.java
@@ -1,19 +1,17 @@
-public class first {
-    public static int firstocur(int[] a, int key,int i){
-        
-        if(i==a.length){
+public class last {
+    public static int lastrec(int[] a, int key, int i){
+        if(i==0){
             return -1;
         }
         if(a[i]==key){
             return i;
         }
-        return firstocur(a, key,i+1);
+        return lastrec(a, key, i-1);
     }
-    
     public static void main(String [] args){
         int [] arr = {10,2,5,8,6,56,8,4,94,3,12,5,8,9,4,55};
-        int i=0;
+        int i=arr.length-1;
         int key = 5;
-        System.out.print(firstocur(arr, key, i));
+        System.out.print(lastrec(arr, key, i));
     }
 }
